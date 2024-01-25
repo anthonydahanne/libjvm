aa3eb52bb1c9201dfe6d39ba4420c7060a9fec6b
Anthony Dahanne <anthony.dahanne@gmail.com>
2024-01-25 10:21:37 -0500

    relocate module

--- helper/debug_9_test.go
+++ helper/debug_9_test.go
@@ -21,8 +21,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/anthonydahanne/libjvm/helper"
 	. "github.com/onsi/gomega"
-	"github.com/paketo-buildpacks/libjvm/helper"
 	"github.com/sclevine/spec"
 )
 
